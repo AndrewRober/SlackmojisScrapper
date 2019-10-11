d53764e7e55a544aadae3ccf4d9860bebbbdb939
AndrewRober <inviolable.sorrow@gmail.com>
2019-10-11 09:04:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace SlackmojisScrapper
+{
+    class Program
+    {
+        static void Main(string[] args) =>
+            new Regex("src=[\\'\"]?([^\\'\" >]+)", RegexOptions.Compiled).Matches(new WebClient().DownloadString(@"https://slackmojis.com/"))
+                .Where(link => link.Value.Contains("images")).Select(link => link.Value.Replace("src=\"", string.Empty)).ToList()
+                .ForEach(link =>
+                {
+                    using (WebClient subclient = new WebClient())
+                    {
+                        var fileName = link.Substring(link.LastIndexOf("/") + 1,
+                            link.LastIndexOf("?") - link.LastIndexOf("/") - 1);
+                        subclient.DownloadFile(new Uri(link), $@"imgs\{fileName}");
+                    }
+                });
+    }
+}
